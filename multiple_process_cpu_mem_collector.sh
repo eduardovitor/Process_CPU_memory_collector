e3f5c16c548e90c309b85b8dcafee234b49162ad
Eduardo Vitor <eduardovitor730@gmail.com>
2021-12-23 22:21:32 -0300

    First commit

---
+++ multiple_process_cpu_mem_collector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function multiple_process_cpu_mem_collector(){
+     process_name="firefox"
+     user_name='eduardo+'
+     time_interval=1
+     iterations=10
+     i=1
+     echo "cpu_usage,mem_usage"
+     while (( ${i} <= ${iterations} ))
+     do
+      cpu_usage=`ps aux | grep ${process_name} | awk '{print $3}' | paste -s -d '+' | bc`
+      mem_usage=`ps aux | grep ${process_name} | awk '{print $4}' | paste -s -d '+' | bc`
+      echo "${cpu_usage},${mem_usage}" 
+      sleep $time_interval
+      ((i++))
+     done
+}
+
+multiple_process_cpu_mem_collector
